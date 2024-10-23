0e5fb4a67b3a51935e13f8a1238d0710631b5131
강세현 <gedfgrdt0@gmail.com>
2024-10-23 17:37:03 +0900

    Feat: SecurityConfig 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/global/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.seniorcenter.sapi.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.seniorcenter.sapi.global.security.exceptionhandler.CustomExceptionHandler;
+import com.seniorcenter.sapi.global.security.jwt.JwtAuthorizationFilter;
+import com.seniorcenter.sapi.global.security.jwt.TokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final TokenProvider tokenProvider;
+	private final CustomExceptionHandler customExceptionHandler;
+
+	@Bean
+	public JwtAuthorizationFilter jwtAuthorizationFilter() throws Exception {
+		JwtAuthorizationFilter filter = new JwtAuthorizationFilter(tokenProvider);
+		return filter;
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf((auth) -> auth.disable());
+		http.formLogin((auth) -> auth.disable());
+		http.httpBasic((auth) -> auth.disable());
+		http.sessionManagement((session) -> session
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		);
+		http.authorizeHttpRequests((auth) -> auth
+			.requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+			.requestMatchers("/api/users/login").permitAll()
+			.anyRequest().authenticated()
+		);
+		http.exceptionHandling((handle) -> handle.authenticationEntryPoint(customExceptionHandler));
+		http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
