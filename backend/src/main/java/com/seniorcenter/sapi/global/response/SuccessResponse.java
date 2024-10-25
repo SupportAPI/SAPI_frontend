011c55ff00e13197d6f8df598b6d6c570c5c2477
강세현 <gedfgrdt0@gmail.com>
2024-10-25 15:55:04 +0900

    Fix: SuccessResponse에 timestamp 필드 추가

--- backend/src/main/java/com/seniorcenter/sapi/global/response/SuccessResponse.java
+++ backend/src/main/java/com/seniorcenter/sapi/global/response/SuccessResponse.java
@@ -1,5 +1,7 @@
 package com.seniorcenter.sapi.global.response;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 
 @Getter
@@ -8,9 +10,11 @@ public class SuccessResponse {
 	private final boolean success = true;
 	private final int status;
 	private final Object data;
+	private final LocalDateTime timestamp;
 
 	public SuccessResponse(int status, Object data) {
 		this.status = status;
 		this.data = data;
+		this.timestamp = LocalDateTime.now();
 	}
 }
