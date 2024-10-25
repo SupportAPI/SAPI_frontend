4f76b8eeabe8b0d0d11ec648ecfb8fd5d97c8bf5
강세현 <gedfgrdt0@gmail.com>
2024-10-25 15:54:33 +0900

    Feat: AuthService, UserService 인터페이스 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/user/service/UserService.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/user/service/UserService.java
@@ -1,32 +1,15 @@
 package com.seniorcenter.sapi.domain.user.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.seniorcenter.sapi.domain.user.domain.User;
-import com.seniorcenter.sapi.domain.user.domain.repository.UserRepository;
+import com.seniorcenter.sapi.domain.user.presentation.dto.request.NewPasswordRequestDto;
+import com.seniorcenter.sapi.domain.user.presentation.dto.request.SendCodeRequestDto;
+import com.seniorcenter.sapi.domain.user.presentation.dto.request.VerifyCodeRequestDto;
 import com.seniorcenter.sapi.domain.user.presentation.dto.response.UserInfoResponseDto;
-import com.seniorcenter.sapi.global.error.exception.CustomException;
-import com.seniorcenter.sapi.global.error.exception.MainException;
-import com.seniorcenter.sapi.global.utils.user.UserUtils;
 
-import lombok.RequiredArgsConstructor;
+public interface UserService {
 
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class UserService {
-
-	private final UserRepository userRepository;
-	private final UserUtils userUtils;
-
-	public UserInfoResponseDto getUserInfo(Long userId) {
-		User user = userUtils.getUserFromSecurityContext();
-
-		if (!user.getId().equals(userId)) {
-			throw new MainException(CustomException.ACCESS_DENIED_EXCEPTION);
-		}
-
-		return new UserInfoResponseDto(user.getId(), user.getEmail(), user.getNickname(), user.getProfileImage());
-	}
+	UserInfoResponseDto getUserInfo(Long userId);
+	boolean checkDuplicateEmail(String email);
+	void sendEmailCode(SendCodeRequestDto sendCodeRequestDto);
+	void verifyEmailCode(VerifyCodeRequestDto verifyCodeRequestDto);
+	void updatePassword(Long userId, NewPasswordRequestDto newPasswordRequestDto);
 }
