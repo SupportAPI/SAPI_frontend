e0359e13a253bc473dfc205cbab6939077812c86
강세현 <gedfgrdt0@gmail.com>
2024-10-29 11:12:42 +0900

    Fix: WorkspaceRepository 엔티티 PK UUID로 변경

--- backend/src/main/java/com/seniorcenter/sapi/domain/workspace/domain/repository/WorkSpaceRepository.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/workspace/domain/repository/WorkSpaceRepository.java
@@ -1,8 +1,10 @@
 package com.seniorcenter.sapi.domain.workspace.domain.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.seniorcenter.sapi.domain.workspace.domain.Workspace;
 
-public interface WorkSpaceRepository extends JpaRepository<Workspace, Long> {
+public interface WorkSpaceRepository extends JpaRepository<Workspace, UUID> {
 }
