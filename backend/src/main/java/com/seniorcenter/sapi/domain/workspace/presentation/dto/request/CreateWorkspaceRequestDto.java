aef6c42204075ee03e88a048e795d27a7ee5110b
강세현 <gedfgrdt0@gmail.com>
2024-10-31 11:21:43 +0900

    Feat: CreateWorkspaceRequestDto size validation 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/workspace/presentation/dto/request/CreateWorkspaceRequestDto.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/workspace/presentation/dto/request/CreateWorkspaceRequestDto.java
@@ -1,10 +1,11 @@
 package com.seniorcenter.sapi.domain.workspace.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateWorkspaceRequestDto(
-	@NotBlank String projectName,
-	@NotBlank String description,
-	@NotBlank String domain
+	@NotBlank @Size(max = 30) String projectName,
+	@NotBlank @Size(max = 255) String description,
+	@NotBlank @Size(max = 255) String domain
 ) {
 }
