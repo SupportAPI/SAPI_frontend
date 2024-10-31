d0809fe2ddf81f6af58418788a65732d5a08ba4f
김문희 <homoonshi@gmail.com>
2024-10-31 16:24:26 +0900

    Feat: 환경변수 페이지  Type 드롭다운 추가

--- src/pages/Environment/Environment.jsx
+++ src/pages/Environment/Environment.jsx
@@ -161,6 +161,41 @@ const DraggableRow = ({ environment, index, moveRow, handleIsChecked, handleType
     );
 };
 
+const DropdownMenu = ({ onClose, position, setIsSecreted }) => {
+    const menuRef = useRef(null);
+
+    const sendIsSecreted = (isSecreted) => {
+        setIsSecreted(isSecreted);
+        onClose();
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                onClose();
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [onClose]);
+
+    return (
+        <ul
+            ref={menuRef}
+            className="absolute bg-white shadow-lg w-32 text-left z-50 border border-gray-200"
+            style={{ top: position.top, left: position.left }}
+        >
+            <li className="p-2 hover:bg-gray-100 cursor-pointer text-center" onClick={() => sendIsSecreted(true)}>Secret</li>
+            <li className="p-2 hover:bg-gray-100 cursor-pointer text-center" onClick={() => sendIsSecreted(false)}>Default</li>
+        </ul>
+    );
+};
+
+
+
 const Environment = () => {
     const [activeTypeId, setActiveTypeId] = useState(null);
     const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
