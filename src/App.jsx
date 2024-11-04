482c06c8d7f688955874a616cf6841a019a7c858
homoonshi <homoonshi@gmail.com>
2024-11-04 23:28:21 +0900

    Feat: SSE 구독 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,49 @@ import AppRoutes from './routes/AppRoutes';
 import useAuthStore from './stores/useAuthStore';
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
-import { ToastContainer, toast, cssTransition } from "react-toastify";
-import { AiOutlineClose } from "react-icons/ai";
+import { ToastContainer, toast, cssTransition } from 'react-toastify';
+import { AiOutlineClose } from 'react-icons/ai';
 import 'react-toastify/dist/ReactToastify.css';
 import 'animate.css';
 
 const fadeOut = cssTransition({
-  enter: "animate__animated animate__fadeIn",
-  exit: "animate__animated animate__fadeOut"
+  enter: 'animate__animated animate__fadeIn',
+  exit: 'animate__animated animate__fadeOut',
 });
 
 import { getToken } from './utils/cookies';
 
 const App = () => {
+  
   const setUserId = useAuthStore((state) => state.setUserId);
+  const none = useState(null);
+
+  useEffect(()=>{
+    const eventSource = new EventSourcePolyfill(
+      '${baseUrl}/api/notifications/connect',{
+        headers: {
+          'Authorization' : getToken();
+        }
+      }
+    )
+  }, [none]);
 
   const notify = () => {
     toast(
-        <div>
-            <h4 className="font-bold text-white text-xl">유저 정보 조회</h4>
-            <p className="text-white">유저 정보 조회 API가 생성되었습니다.</p>
-        </div>,
-        {
-            icon: false,
-            className: 'p-3 bg-[#4F5A66] text-white rounded-2xl',
-            closeButton: <CloseButton/>,
-        }
-    );  
+      <div>
+        <h4 className='font-bold text-white text-xl'>유저 정보 조회</h4>
+        <p className='text-white'>유저 정보 조회 API가 생성되었습니다.</p>
+      </div>,
+      {
+        icon: false,
+        className: 'p-3 bg-[#4F5A66] text-white rounded-2xl',
+        closeButton: <CloseButton />,
+      }
+    );
   };
 
   const CloseButton = ({ closeToast }) => (
-    <AiOutlineClose className="text-white cursor-pointer" 
-    onClick={closeToast}
-    size={24} />
+    <AiOutlineClose className='text-white cursor-pointer' onClick={closeToast} size={24} />
   );
 
   useEffect(() => {
@@ -53,16 +63,18 @@ const App = () => {
 
   return (
     <>
-      <button style={{display:"none"}} onClick={notify}>Show Toast</button>
+      <button style={{ display: 'none' }} onClick={notify}>
+        Show Toast
+      </button>
       <AppRoutes />
       <ToastContainer
-                position="bottom-right"
-                autoClose={5000}
-                hideProgressBar
-                closeOnClick
-                pauseOnHover
-                transition={fadeOut}
-                toastClassName="flex items-center justify-between p-4 shadow-lg rounded-2xl"
+        position='bottom-right'
+        autoClose={5000}
+        hideProgressBar
+        closeOnClick
+        pauseOnHover
+        transition={fadeOut}
+        toastClassName='flex items-center justify-between p-4 shadow-lg rounded-2xl'
       />
     </>
   );
