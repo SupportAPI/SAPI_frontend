d0c2081933cd1b9bcb609e6605839722a9fa839b
강세현 <gedfgrdt0@gmail.com>
2024-11-04 15:54:31 +0900

    Feat: NotificationType 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/domain/notification/domain/NotificationType.java
@@ -0,0 +1,17 @@
+package com.seniorcenter.sapi.domain.notification.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public enum NotificationType {
+
+	API_CONFIRM("API_CONFIRM"),
+	API_UPDATE("API_UPDATE"),
+	TEST_FAIL("TEST_FAIL"),
+	COMMENT_TAG("COMMENT_TAG");
+
+	private final String type;
+}
