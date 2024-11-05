58e72ebf7b5dcf4e81532d2945347873bdbd43df
김문희 <homoonshi@gmail.com>
2024-11-05 12:22:13 +0900

    Fix: info 수정

--- src/pages/docs/Info.jsx
+++ src/pages/docs/Info.jsx
@@ -1,16 +1,11 @@
-
-
 const Info = () => {
-
-    return (
-        <div className="flex flex-col">
-            <p className="">ID</p>
-            <p className="">Created</p>
-            <p className="">Last Updated</p>
-        </div>
-    );
-
-}
-
+  return (
+    <div className='flex flex-col'>
+      <p className=''>ID</p>
+      <p className=''>Created</p>
+      <p className=''>Last Updated</p>
+    </div>
+  );
+};
 
 export default Info;
