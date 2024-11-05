f85890dceffd1cec43905f4265462bcf6edd0aa1
박용빈 <pyb0718@naver.com>
2024-11-05 17:56:01 +0900

    Feat : Repository 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/api/domain/repository/ApiQueryParameterRepository.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/api/domain/repository/ApiQueryParameterRepository.java
@@ -1,6 +1,5 @@
 package com.seniorcenter.sapi.domain.api.domain.repository;
 
-import com.seniorcenter.sapi.domain.api.domain.ApiCookie;
 import com.seniorcenter.sapi.domain.api.domain.ApiQueryParameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
