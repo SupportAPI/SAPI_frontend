86d32f9e8d25f7751b7ce1a7c670138b5de94de2
강세현 <gedfgrdt0@gmail.com>
2024-11-06 14:53:09 +0900

    Feat: NotificationType type 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/notification/domain/NotificationType.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/notification/domain/NotificationType.java
@@ -11,7 +11,8 @@ public enum NotificationType {
 	API_CONFIRM("API_CONFIRM"),
 	API_UPDATE("API_UPDATE"),
 	TEST_FAIL("TEST_FAIL"),
-	COMMENT_TAG("COMMENT_TAG");
+	COMMENT_TAG("COMMENT_TAG"),
+	WORKSPACE_INVITE("WORKSPACE_INVITE");
 
 	private final String type;
 }
