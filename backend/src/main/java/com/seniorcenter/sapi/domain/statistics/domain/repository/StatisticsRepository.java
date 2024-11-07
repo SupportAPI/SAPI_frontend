78a6b2a254dd286af071263a2986088622639c27
강세현 <gedfgrdt0@gmail.com>
2024-11-07 11:22:49 +0900

    Feat: StatisticsRepository findByWorkspaceIdAndUserIdAndDate 메서드 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/domain/statistics/domain/repository/StatisticsRepository.java
@@ -0,0 +1,14 @@
+package com.seniorcenter.sapi.domain.statistics.domain.repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.seniorcenter.sapi.domain.statistics.domain.Statistics;
+
+public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+
+	Optional<Statistics> findByWorkspaceIdAndUserIdAndDate(UUID workspaceId, Long userId, LocalDate date);
+}
