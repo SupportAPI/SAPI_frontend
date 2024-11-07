a097528a5ca0de60fc2012fcb05e9004849220c6
강세현 <gedfgrdt0@gmail.com>
2024-11-07 15:25:45 +0900

    Feat: StatisticsService 개발률 조회 기능 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/statistics/domain/repository/StatisticsRepository.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/statistics/domain/repository/StatisticsRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.seniorcenter.sapi.domain.statistics.domain.Statistics;
 
-public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+public interface StatisticsRepository extends JpaRepository<Statistics, Long>, StatisticsRepositoryCustom {
 
 	Optional<Statistics> findByWorkspaceIdAndUserIdAndDate(UUID workspaceId, Long userId, LocalDate date);
 }
