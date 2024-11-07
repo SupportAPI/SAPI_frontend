4225f670d21d5fd42538cdc413ee82d3cd51e222
강세현 <gedfgrdt0@gmail.com>
2024-11-07 15:25:19 +0900

    Feat: SpecificationRepositoryCustom api 개발 진행률 조회 기능 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/specification/domain/repository/SpecificationRepositoryCustom.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/specification/domain/repository/SpecificationRepositoryCustom.java
@@ -6,6 +6,11 @@ import java.util.UUID;
 import com.seniorcenter.sapi.domain.statistics.presentation.dto.ConfirmedApiCountDto;
 
 public interface SpecificationRepositoryCustom {
-
 	List<ConfirmedApiCountDto> countConfirmedApiByManager(UUID workspaceId);
+	Long countTotalSpecifications(UUID workspaceId);
+	Long countLocalSuccessSpecifications(UUID workspaceId);
+	Long countServerSuccessSpecifications(UUID workspaceId);
+	Long countTotalSpecificationsByUser(UUID workspaceId, Long userId);
+	Long countLocalSuccessSpecificationsByUser(UUID workspaceId, Long userId);
+	Long countServerSuccessSpecificationsByUser(UUID workspaceId, Long userId);
 }
