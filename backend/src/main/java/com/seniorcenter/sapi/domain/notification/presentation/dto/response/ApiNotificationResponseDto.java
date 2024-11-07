44b088ab1e5d980eb70a76ad344b5c99fa9565d8
강세현 <gedfgrdt0@gmail.com>
2024-11-07 15:40:47 +0900

    Feat: ApiNotificationResponseDto 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/domain/notification/presentation/dto/response/ApiNotificationResponseDto.java
@@ -0,0 +1,15 @@
+package com.seniorcenter.sapi.domain.notification.presentation.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ApiNotificationResponseDto(
+	Long id,
+	UUID fromId,
+	UUID workspaceId,
+	String fromName,
+	String message,
+	String notificationType,
+	LocalDateTime createdDatetime
+) {
+}
