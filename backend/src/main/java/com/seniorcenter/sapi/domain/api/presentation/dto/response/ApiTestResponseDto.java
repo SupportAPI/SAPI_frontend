471f58837b302a109668181ed0613c0c16ab964b
강세현 <gedfgrdt0@gmail.com>
2024-11-08 10:59:43 +0900

    Feat: api 테스트 목록 ApiTestResponseDto 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/domain/api/presentation/dto/response/ApiTestResponseDto.java
@@ -0,0 +1,19 @@
+package com.seniorcenter.sapi.domain.api.presentation.dto.response;
+
+import java.util.UUID;
+
+import com.seniorcenter.sapi.domain.api.domain.enums.HttpMethod;
+import com.seniorcenter.sapi.domain.specification.domain.TestStatus;
+
+public record ApiTestResponseDto(
+	UUID id,
+	String category,
+	String name,
+	HttpMethod method,
+	String path,
+	String description,
+	String managerName,
+	TestStatus localTest,
+	TestStatus serverTest
+) {
+}
