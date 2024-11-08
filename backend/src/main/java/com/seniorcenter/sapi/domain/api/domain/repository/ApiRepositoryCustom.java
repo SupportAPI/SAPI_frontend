3c2d897fd886c92b11a76e28950de25ea8df202f
강세현 <gedfgrdt0@gmail.com>
2024-11-08 11:00:19 +0900

    Feat: ApiRepositoryCustom api 테스트 목록 조회 기능 추가

---
+++ backend/src/main/java/com/seniorcenter/sapi/domain/api/domain/repository/ApiRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.seniorcenter.sapi.domain.api.domain.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.seniorcenter.sapi.domain.api.presentation.dto.response.ApiTestResponseDto;
+
+public interface ApiRepositoryCustom {
+	List<ApiTestResponseDto> findConfirmedApisByWorkspaceId(UUID workspaceId);
+}
