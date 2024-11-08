b7a114edccad115be30b4ea20288547e69c3452b
강세현 <gedfgrdt0@gmail.com>
2024-11-08 11:03:04 +0900

    Feat: ApiController api 테스트 목록 조회 기능 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/api/presentation/ApiController.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/api/presentation/ApiController.java
@@ -30,12 +30,12 @@ public class ApiController {
         return apiService.getApiByApiId(workspaceId, apiId);
     }
 
-    @GetMapping("/workspaces/{workspaceId}/api-tests")
+    @GetMapping("/workspace/{workspaceId}/api-tests")
     public List<ApiTestResponseDto> getTestApis(@PathVariable("workspaceId") UUID workspaceId) {
         return apiService.getConfirmedApisByWorkspaceId(workspaceId);
     }
 
-    @GetMapping("/workspaces/{workspaceId}/api-tests/{apiId}")
+    @GetMapping("/workspace/{workspaceId}/api-tests/{apiId}")
     public ApiTestDetailResponseDto getTestApi(@PathVariable("workspaceId") UUID workspaceId, @PathVariable("apiId") UUID apiId) {
         return null;
     }
