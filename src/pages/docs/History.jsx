0933433c2cf2c62643c94eccb56d07f247dc8e66
homoonshi <homoonshi@gmail.com>
2024-11-09 21:15:15 +0900

    Feat: API History CSS 추가

---
+++ src/pages/docs/History.jsx
@@ -0,0 +1,103 @@
+import { FaEllipsisH } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+
+const History = ({ openHistoryDetail }) => {
+  const data = [
+    {
+      createdDate: '2024-11-09T12:00:00.000Z',
+      apiId: 'fa9f27d8-28a2-47b5-9877-35c155823b57',
+      nickname: 'user1',
+    },
+    {
+      createdDate: '2024-11-03T12:00:00.000Z',
+      apiId: 'c7e24d7f-1234-4b56-9876-78a212344def',
+      nickname: 'user2',
+    },
+    {
+      createdDate: '2024-11-09T12:00:00.000Z',
+      apiId: 'b4a123d8-56f8-4a7d-bb44-12a34b7c8de9',
+      nickname: 'user3',
+    },
+    {
+      createdDate: '2024-11-09T12:00:00.000Z',
+      apiId: 'd9f237b8-1234-4f98-a77b-43c255823f45',
+      nickname: 'user4',
+    },
+    {
+      createdDate: '2024-11-09T12:00:00.000Z',
+      apiId: 'e8a237c9-7890-4d7e-b12f-56d3448b9a12',
+      nickname: 'user5',
+    },
+    {
+      createdDate: '2024-11-09T12:00:00.000Z',
+      apiId: 'f5a237d1-1234-4a9c-bb77-98c144823d56',
+      nickname: 'user6',
+    },
+    {
+      createdDate: '2024-11-03T12:00:00.000Z',
+      apiId: 'a1b237c8-5678-4e7d-b89f-34b244823e78',
+      nickname: 'user7',
+    },
+    {
+      createdDate: '2024-11-02T12:00:00.000Z',
+      apiId: 'c6e347d7-4567-4f5b-987c-12a444823f89',
+      nickname: 'user8',
+    },
+    {
+      createdDate: '2024-11-01T12:00:00.000Z',
+      apiId: 'd2f347d9-6789-4d5c-a12b-67d244823a90',
+      nickname: 'user9',
+    },
+    {
+      createdDate: '2024-10-09T12:00:00.000Z',
+      apiId: 'b3a247e1-7890-4a5b-c98f-89d124823e34',
+      nickname: 'user10',
+    },
+  ];
+
+  const handleHistory = (apiId) => {
+    console.log(apiId);
+  };
+
+  return (
+    <div className='w-full flex flex-col mb-4 overflow-y-auto sidebar-scrollbar'>
+      <p className='text-2xl font-bold mb-4'>API History</p>
+      <hr className='border-t border-gray-300 mb-3 w-full' />
+      {data?.length === 0 ? (
+        <p className='text-[#666666]'>No History</p>
+      ) : (
+        data
+          .slice()
+          .sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate))
+          .map((history) => (
+            <div
+              key={history.apiId}
+              onClick={() => openHistoryDetail(history.apiId)}
+              className='w-full flex flex-row text-[#666666] mt-4 border rounded-lg'
+            >
+              <div className='w-full flex flex-col ml-1 p-3'>
+                <div className='flex flex-row justify-between'>
+                  <div className='flex flex-row'>
+                    <p>명세 확정일 : </p>
+                    <span className='text-sm mx-1 mt-0.5'>
+                      {new Date(history.createdDate).toLocaleString('ko-KR', {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </span>
+                  </div>
+                  <FaEllipsisH className='cursor-pointer mt-1' size={16} onClick={() => handleHistory(history.apiId)} />
+                </div>
+                <p>명세 확정자 : {history.nickname}</p>
+              </div>
+            </div>
+          ))
+      )}
+    </div>
+  );
+};
+
+export default History;
---
+++ src/pages/docs/HistoryDetail.jsx
@@ -0,0 +1,305 @@
+import { useEffect, useState, useRef } from 'react';
+
+import { FiChevronDown, FiMessageSquare, FiCode, FiFileText, FiX } from 'react-icons/fi';
+import { FaDownload, FaSave, FaShareAlt, FaTrashAlt, FaInfoCircle, FaHistory } from 'react-icons/fa';
+
+import Parameters from '../docs/Parameters';
+import Request from '../docs/Request';
+import Response from '../docs/Response';
+
+const HistoryDetail = ({ apiId, closeHistoryDetail }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [activeLeftTab, setActiveLeftTab] = useState('parameters');
+  const [apiDetail, setApiDetail] = useState(null);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const dropdownRef = useRef(null);
+  const categoryRef = useRef(null);
+  const nameRef = useRef(null);
+
+  const methodStyles = {
+    GET: 'text-blue-500',
+    POST: 'text-green-500',
+    PUT: 'text-orange-500',
+    PATCH: 'text-purple-500',
+    DELETE: 'text-red-500',
+    HEAD: 'text-gray-500',
+    OPTIONS: 'text-yellow-500',
+  };
+
+  const dummyData = {
+    id: 19,
+    category: {
+      value: 'asd1',
+    },
+    createdDate: '2024-11-07T14:14:46.182551',
+    description: 'adasdasdasd',
+    docId: '01b1c7c9-c747-49ae-a449-4116f9c4d0a3',
+    lastModifyDate: '2024-11-07T14:14:46.180635',
+    managerEmail: 'asd',
+    managerName: 'asd',
+    managerProfileImage: '',
+    method: 'GET',
+    name: 'asd13',
+    parameters: {
+      authType: 'BEARER',
+      cookies: [],
+      headers: [],
+      queryParameters: [],
+    },
+    path: 'asdassd',
+    request: {
+      bodyType: 'JSON',
+      formData: [],
+      json: {
+        jsonDataDescription: null,
+        jsonDataId: '44',
+        jsonDataKey: 'json',
+        jsonDataType: 'JSON',
+        jsonDataValue: '{"whn":"asdasd", "123":"whn", "asdasd":"Qweqwe"}',
+      },
+    },
+    responses: {
+      200: [],
+    },
+  };
+
+  const adjustWidth = (target, ref) => {
+    ref.current.style.width = '0px';
+    ref.current.style.width = `${target.scrollWidth}px`;
+  };
+
+  const handleCategoryChange = (e) => {
+    setApiDetail((prev) => ({ ...prev, category: e.target.value }));
+    adjustWidth(e.target, categoryRef);
+  };
+
+  const handleNameChange = (e) => {
+    setApiDetail((prev) => ({ ...prev, name: e.target.value }));
+    adjustWidth(e.target, nameRef);
+  };
+
+  const handleMethodSelect = (selectedMethod) => {
+    setApiDetail((prev) => ({ ...prev, method: selectedMethod }));
+    setShowDropdown(false);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setApiDetail((prev) => ({ ...prev, description: e.target.value }));
+  };
+
+  const handleApiUrlChange = (e) => {
+    setApiDetail((prev) => ({ ...prev, path: e.target.value }));
+  };
+
+  const handleDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
+  useEffect(() => {
+    if (categoryRef.current) adjustWidth(categoryRef.current, categoryRef);
+    if (nameRef.current) adjustWidth(nameRef.current, nameRef);
+  }, [dummyData]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  // paramsChange 함수 정의
+  const handleParamsChange = (newParams) => {
+    setApiDetail((prevDetail) => ({
+      ...prevDetail,
+      parameters: newParams,
+    }));
+    console.log('updated', apiDetail?.parameters);
+    // console.log('Updated Parameters:', newParams);
+  };
+
+  const handleRequestChange = (newRequest) => {
+    setApiDetail((prevDetail) => ({
+      ...prevDetail,
+      request: newRequest,
+    }));
+    // console.log('Updated Request:', newRequest);
+  };
+
+  const handleResponseChange = (updatedResponse) => {
+    setApiDetail((prevDetail) => ({
+      ...prevDetail,
+      response: updatedResponse,
+    }));
+    // console.log('Updated Response:', updatedResponse);
+  };
+
+  return (
+    <>
+      <button onClick={closeHistoryDetail} className='text-gray-600 hover:text-gray-800'>
+        닫기
+      </button>
+      <h2 className='text-2xl font-bold mb-4'>History Detail - {dummyData.name}</h2>
+      <div className='flex flex-1 flex-col'>
+        <div className='flex items-baseline space-x-1 mb-8 justify-between'>
+          <div className='inline-flex items-baseline space-x-1'>
+            <input
+              type='text'
+              ref={categoryRef}
+              className='border-b focus:outline-none w-auto max-w-[200px] text-[18px] px-2'
+              placeholder='Enter Category'
+              value={dummyData?.category.value || ''}
+              readOnly
+            />
+            <span className='text-gray-400' style={{ margin: '0 12px' }}>
+              /
+            </span>
+
+            <input
+              type='text'
+              ref={nameRef}
+              className='text-2xl border-b focus:outline-none w-auto max-w-[250px] px-2 text-[18px]'
+              value={dummyData?.name || ''}
+              readOnly
+              placeholder='Enter API Name'
+            />
+          </div>
+
+          <div className='flex space-x-4'>
+            <button className='flex items-center h-8 text-[14px] space-x-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 px-2 rounded-md'>
+              <FaSave />
+              <span>Save</span>
+            </button>
+            <button
+              onClick={handleDeleteClick}
+              className='flex items-center h-8 text-[14px] space-x-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 px-2 rounded-md'
+            >
+              <FaTrashAlt />
+              <span>Delete</span>
+            </button>
+            <button className='flex items-center h-8 text-[14px] space-x-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 px-2 rounded-md'>
+              <FaDownload />
+              <span>Export</span>
+            </button>
+            <button className='flex items-center h-8 text-[14px] space-x-2 text-gray-600 hover:text-gray-800 hover:bg-gray-200 px-2 rounded-md'>
+              <FaShareAlt />
+              <span>Share</span>
+            </button>
+          </div>
+        </div>
+
+        {/* 삭제 모달 */}
+        {showDeleteModal && (
+          <div className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50'>
+            <div className='bg-white p-6 rounded-lg shadow-lg w-80'>
+              <h3 className='text-xl font-bold mb-4'>삭제하시겠습니까?</h3>
+              <p className='mb-6'>현재 API 문서를 삭제하시겠습니까?</p>
+              <div className='flex justify-end space-x-4'>
+                <button onClick={handleCloseModal} className='px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300'>
+                  취소
+                </button>
+                <button
+                  onClick={handleConfirmDelete}
+                  className='px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700'
+                >
+                  삭제
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* API Path and Description */}
+        <div className='mb-4'>
+          <label className='block text-[18px] font-semibold mb-2'>API Path</label>
+          <div className='relative'>
+            <div className='flex items-center space-x-2'>
+              <button
+                className={`px-4 py-2 w-[150px] rounded-md border ${
+                  methodStyles[dummyData.method]
+                } border-gray-300 h-10`}
+                onClick={() => setShowDropdown((prev) => !prev)}
+              >
+                <div className='flex justify-between items-center'>
+                  <span>{dummyData.method}</span>
+                  <FiChevronDown className='ml-2' color='black' />
+                </div>
+              </button>
+
+              <input
+                type='text'
+                className='border rounded px-2 py-1 flex-grow h-10'
+                placeholder='Enter URL'
+                value={dummyData.path}
+                readOnly
+              />
+            </div>
+
+            {showDropdown && (
+              <div
+                ref={dropdownRef}
+                className='absolute bg-white border mt-1 rounded shadow-md z-10'
+                style={{ top: '100%', left: 0, width: '150px' }}
+              >
+                {['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'].map((method) => (
+                  <div
+                    key={method}
+                    className={`p-2 cursor-pointer ${methodStyles[method]} hover:bg-gray-100`}
+                    onClick={() => handleMethodSelect(method)}
+                  >
+                    {method}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className='mb-4'>
+          <label className='block font-semibold mb-2 text-[18px]'>Description</label>
+          <textarea
+            className='border rounded w-full p-2'
+            placeholder='No Description.'
+            value={dummyData.description}
+            readOnly
+            style={{ resize: 'none' }}
+          />
+        </div>
+
+        {/* Left Tabs for Parameters, Request, Response */}
+        <div className='border-b mb-4'>
+          <nav className='flex space-x-4'>
+            {['parameters', 'request', 'response'].map((tab) => (
+              <a
+                key={tab}
+                href='#'
+                className={`px-2 py-1 ${activeLeftTab === tab ? 'border-b-2 border-indigo-600' : 'text-gray-500'}`}
+                onClick={() => setActiveLeftTab(tab)}
+              >
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              </a>
+            ))}
+          </nav>
+        </div>
+
+        <div>
+          {activeLeftTab === 'parameters' && (
+            <Parameters paramsChange={handleParamsChange} initialValues={dummyData.parameters} />
+          )}
+          {activeLeftTab === 'request' && (
+            <Request requestChange={handleRequestChange} initialValues={dummyData.request} />
+          )}
+          {activeLeftTab === 'response' && (
+            <Response responseChange={handleResponseChange} initialValues={dummyData.responses[200]} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HistoryDetail;
