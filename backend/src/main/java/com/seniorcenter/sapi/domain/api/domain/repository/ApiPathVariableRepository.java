add816aed4483670ee6713e8bf1df2ba37823adb
박용빈 <pyb0718@naver.com>
2024-11-13 09:19:44 +0900

    Feat : API 상세 조회 관련 기능 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/api/domain/repository/ApiPathVariableRepository.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/api/domain/repository/ApiPathVariableRepository.java
@@ -1,8 +1,11 @@
 package com.seniorcenter.sapi.domain.api.domain.repository;
 
+import com.seniorcenter.sapi.domain.api.domain.Api;
+import com.seniorcenter.sapi.domain.api.domain.ApiPathVariable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.seniorcenter.sapi.domain.api.domain.ApiPathVariable;
+import java.util.List;
 
 public interface ApiPathVariableRepository extends JpaRepository<ApiPathVariable, Long> {
+    List<ApiPathVariable> findAllByApi(Api api);
 }
