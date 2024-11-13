fb2bf2b4cc55ccb6aca1b9310bf516b444713c56
JYPJUN <psw9502@naver.com>
2024-11-13 12:59:40 +0900

    Feat: index.html 최상단 이미지 변경 및 title 이름 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        black: '#000000',
+      },
       fontFamily: {
         sans: ['Pretendard-Regular', 'sans-serif'],
       },
