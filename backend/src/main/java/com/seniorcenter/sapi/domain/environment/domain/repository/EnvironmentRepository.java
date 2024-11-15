4ab9c0e935efa55183feb18ee4dfd0763fe5e1b7
강세현 <gedfgrdt0@gmail.com>
2024-11-15 09:56:29 +0900

    Feat: EnvironmentRepository 카테고리 내 동일 variable 여부 조회 기능 추가

--- backend/src/main/java/com/seniorcenter/sapi/domain/environment/domain/repository/EnvironmentRepository.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/environment/domain/repository/EnvironmentRepository.java
@@ -3,6 +3,8 @@ package com.seniorcenter.sapi.domain.environment.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.seniorcenter.sapi.domain.environment.domain.Environment;
+import com.seniorcenter.sapi.domain.environment.domain.EnvironmentCategory;
 
 public interface EnvironmentRepository extends JpaRepository<Environment, Long> {
+	boolean existsByEnvironmentCategoryAndVariable(EnvironmentCategory environmentCategory, String variable);
 }
