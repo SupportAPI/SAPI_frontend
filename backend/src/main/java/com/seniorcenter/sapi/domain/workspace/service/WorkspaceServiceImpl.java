b1669a8a5cdfdd6289f3c94ee1faa2bb0357b552
chpark6851 <chpark6851@koreatech.ac.kr>
2024-11-16 21:43:09 +0900

    Fix : 현재 workspace 접속 인원 조회시 구독 경로 수정 - 기존경로 -> /ws/sub/기존경로

--- backend/src/main/java/com/seniorcenter/sapi/domain/workspace/service/WorkspaceServiceImpl.java
+++ backend/src/main/java/com/seniorcenter/sapi/domain/workspace/service/WorkspaceServiceImpl.java
@@ -87,9 +87,10 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 		Workspace workspace = workSpaceRepository.findById(workspaceId)
 			.orElseThrow(() -> new MainException(CustomException.NOT_FOUND_WORKSPACE));
 
+        log.info("[COUNT CONNECTIONS] " + "/ws/sub/workspaces/"+ workspace.getId() +"/docs : " + webSocketUtil.countUsersSubscribedToDestination("/workspaces/"+ workspace.getId() +"/docs"));
 		WorkspaceInfoResponseDto workspaceInfoResponseDto = new WorkspaceInfoResponseDto(workspace.getId(),
 			workspace.getProjectName(), workspace.getDescription(), workspace.getMainImage(), workspace.getDomain(),
-			workspace.getIsCompleted(), webSocketUtil.countUsersSubscribedToDestination("/workspaces/"+ workspaceId +"/docs"));
+			workspace.getIsCompleted(), webSocketUtil.countUsersSubscribedToDestination("/ws/sub/workspaces/"+ workspaceId +"/docs"));
 		return workspaceInfoResponseDto;
 	}
 
@@ -101,6 +102,7 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 		return memberships.stream()
 			.map(membership -> {
 				Workspace workspace = membership.getWorkspace();
+				log.info("[COUNT CONNECTIONS] " + "/ws/sub/workspaces/"+ workspace.getId() +"/docs : " + webSocketUtil.countUsersSubscribedToDestination("/workspaces/"+ workspace.getId() +"/docs"));
 				return new WorkspaceInfoResponseDto(
 					workspace.getId(),
 					workspace.getProjectName(),
@@ -108,7 +110,7 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 					workspace.getMainImage(),
 					workspace.getDomain(),
 					workspace.getIsCompleted(),
-					webSocketUtil.countUsersSubscribedToDestination("/workspaces/"+ workspace.getId() +"/docs")
+					webSocketUtil.countUsersSubscribedToDestination("/ws/sub/workspaces/"+ workspace.getId() +"/docs")
 				);
 			})
 			.collect(Collectors.toList());
